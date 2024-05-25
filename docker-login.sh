f2191ace9a890659fed2c660a1b0d1571213dc50
dev <felix@nexumnet.com>
2024-05-25 01:34:25 -0400

    fixed path

--- docker-login.sh
+++ docker-login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # A TOKEN.txt file in the same relative directory is needed.
-cat ../../TOKEN.txt | docker login https://ghcr.io -u USERNAME --password-stdin
+cat TOKEN.txt | docker login https://ghcr.io -u USERNAME --password-stdin
